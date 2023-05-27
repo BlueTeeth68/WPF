26939fcda7db4296edc43cc1ab796115d44592ef
BlueTeeth68 <daominhtri1000@gmail.com>
2023-05-27 11:02:19 +0700

    Add project clone Modern UI

---
+++ Sample Project/Flat UI Project/ModernDesign/ModernDesign/Core/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernDesign.Core
+{
+    internal class RelayCommand : ICommand
+    {
+
+        private Action<object> _execute;
+        private Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
